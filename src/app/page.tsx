247198fa19ba034cb77c7ab3e3fe5c7544b0666c
Pintu kumar <pintu1012kumar@gmail.com>
2025-04-02 00:05:02 +0530

    updates components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
 import Dashboard from "./pages/Dashboard";
-import { cn } from "./lib/utils";
-import { BackgroundBeamsWithCollisionDemo } from "./pages/home";
 import { SignupFormDemo } from "./pages/signup";
 
 
 export default function Home() {
   return (
-    
     <div>
       <Dashboard/>
-     <SignupFormDemo/>
-      
+     <SignupFormDemo/>  
     </div>
   );
 }
